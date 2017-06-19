1f91b597a308d1e69a76163fb6da25b2f441b2c3
Nagrajnag <nagrajnag2014@gmail.com>
2017-06-19 11:03:48 -0500

    Registration form assignment

---
+++ imcs_assignment4/imcs_assignment4/WebForm2.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="imcs_assignment4.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:Label ID="Label1" runat="server" Text="Name :"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Age :"></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label3" runat="server" Text="Email :"></asp:Label>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        </p>
+         <p>
+            <asp:Label ID="conform" runat="server" Text="submitted!!!" Visible ="false"></asp:Label>
+           
+        </p>
+
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="submit" />
+        </p>
+        <p>
+             <asp:Label ID="lblmessage" runat="server" >
+            
+            </asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ imcs_assignment4/imcs_assignment4/WebForm2.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Data;
+using System.Web.UI.WebControls;
+
+namespace imcs_assignment4
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        private object reader;
+        private object lblmessage;
+        private object confirm;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+            Response.Write( "Page load event handled. <br />");
+
+            if (Page.IsPostBack)
+            {
+              Response.Write("Page post back event handled.<br/>");
+            }
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            Response.Write( "Page initialization event handled.<br/>");
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            Response.Write( "Page prerender event handled. <br/>");
+        }
+        protected void page_preinit(object sender, EventArgs e)
+        {
+            Response.Write("Page preinit event handled. <br/>");
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TextWriter txt = new StreamWriter(@"C:\Users\Samir Panigrahi\Documents\Visual Studio 2015\Projects\demo.txt", true);
+            txt.WriteLine(TextBox1.Text,"\n" );
+            txt.WriteLine(TextBox2.Text, "\n");
+            txt.WriteLine(TextBox3.Text, "\n");
+            conform.Visible = true;
+            txt.Close();
+        }
+    }
+}
