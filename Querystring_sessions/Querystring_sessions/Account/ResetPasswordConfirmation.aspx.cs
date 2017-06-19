c84d80fc285509cf77b6e03b6e688b0484f8fa58
Nagrajnag <nagrajnag2014@gmail.com>
2017-06-19 10:43:13 -0500

    QuerystringAssignment

---
+++ Querystring_sessions/Querystring_sessions/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Querystring_sessions.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Querystring_sessions/Querystring_sessions/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Querystring_sessions.Startup))]
+namespace Querystring_sessions
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
